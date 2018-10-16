4fe25bddae5e5a11b0dcdf7eee50a72b044fbeef
Ted Huntington <thunting@uci.edu>
2018-10-16 12:57:43 -0700

    trying to solve init error

--- src/mgos_sgp30.c
+++ src/mgos_sgp30.c
@@ -40,9 +40,9 @@ void sensirion_sleep_usec(uint32_t useconds)
     mgos_msleep((useconds / 1000) + 1);
 }
 
-bool mgos_sgp30_init(void) {
-  return true;
-}
+//bool mgos_sgp30_init(void) {
+//  return true;
+//}
 
 bool mgos_sgp30_setup(void)
 {
